c74daf570ea8d418a108f07601ed695d92685080
AngelNahuat <nglnahuat@gmail.com>
2022-02-17 22:03:59 -0500

    Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,14 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Esta es la pagina de alerta "),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Mostrar alerta"),
+          onPressed: () => _mostrarAler(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.red, shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.abc),
         onPressed: () {
@@ -14,5 +22,32 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _mostrarAler(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text("Titulo"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[Text("Contenido"), FlutterLogo(size: 100.0)],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text("Cancelar"),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              TextButton(
+                child: Text("Ok"),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          );
+        });
   }
 }
