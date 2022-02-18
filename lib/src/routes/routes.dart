59ee951ea9a3a4b9abab4976c4fa1f93ae8079d6
AngelNahuat <nglnahuat@gmail.com>
2022-02-17 23:49:53 -0500

    inputFields

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/input_pages.dart';
 import 'package:flutter/material.dart';
 
 import '../pages/alert_page.dart';
@@ -5,6 +6,7 @@ import '../pages/animated_container.dart';
 import '../pages/avatar_page.dart';
 import '../pages/card_page.dart';
 import '../pages/home_page.dart';
+import '../pages/input_pages.dart';
 
 Map<String, WidgetBuilder> getAppRoutes() {
   return <String, WidgetBuilder>{
@@ -13,5 +15,6 @@ Map<String, WidgetBuilder> getAppRoutes() {
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedconPage(),
+    'inputs': (BuildContext context) => InputPage(),
   };
 }
